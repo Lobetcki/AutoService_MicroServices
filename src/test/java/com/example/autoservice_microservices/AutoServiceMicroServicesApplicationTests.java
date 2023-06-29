7b0972837bfe59ac1cad4553a7d57ccdc7c22268
Lobetcki <Lobetcki@gmail.com>
2023-06-29 11:45:16 +0300

    29.06.2023

---
+++ src/test/java/com/example/autoservice_microservices/AutoServiceMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoservice_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceMicroServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
